28816679363790aabbe3d697309a8f9b8a63dc81
Ivan Rachev <ivan.rachev97@gmail.com>
2018-06-28 19:08:08 +0300

    trying intellij git push

--- RemindMebyTheVisionTeam/src/me/remind/Main.java
+++ RemindMebyTheVisionTeam/src/me/remind/Main.java
@@ -2,7 +2,11 @@ package me.remind;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("This is Main");
+    
+        ClipBoard clipBoard = new ClipBoard();
+        
     }
 }

dd69f2b9e405320abbefaa0ea2ac5524e211bc29
Ivan Rachev <ivan.rachev97@gmail.com>
2018-06-29 16:02:25 +0300

    implementing Remindable interface

--- RemindMebyTheVisionTeam/src/me/remind/TextNote.java
+++ RemindMebyTheVisionTeam/src/me/remind/TextNote.java
@@ -1,21 +1,30 @@
 package me.remind;
 
-public class TextNote extends Note
+public class TextNote extends Note implements Remindable
 {
     private String text;
     
-    TextNote(String title, String deadline, String text) {
+    TextNote(String title, String deadline, String text)
+    {
         super(title, deadline);
         this.text = text;
     }
-
+    
     @Override
-    public void makeNote() {
-
+    public void makeNote()
+    {
+    
     }
-
+    
     @Override
-    public void reviewNote() {
-
+    public void previewNote()
+    {
+    
+    }
+    
+    @Override
+    public void remind()
+    {
+    
     }
 }
--- RemindMebyTheVisionTeam/src/me/remind/VoiceNote.java
+++ RemindMebyTheVisionTeam/src/me/remind/VoiceNote.java
@@ -1,33 +1,44 @@
 package me.remind;
 
-public class VoiceNote extends Note
+public class VoiceNote extends Note implements Remindable
 {
-    private  String audioFile;//
-
-    private String getAudioFile() {
+    private String audioFile;
+    
+    private String getAudioFile()
+    {
         return audioFile;
     }
-
-    private void setAudioFile(String audioFile) {
+    
+    private void setAudioFile(String audioFile)
+    {
         this.audioFile = audioFile;
     }
-
-    public VoiceNote(String title, String deadline, Priority priority, String audioFile) {
+    
+    public VoiceNote(String title, String deadline, Priority priority, String audioFile)
+    {
         this(title, deadline, priority);
         setAudioFile(audioFile);
     }
-
-    public VoiceNote(String title, String deadline, Priority priority) {
+    
+    public VoiceNote(String title, String deadline, Priority priority)
+    {
         super(title, deadline, priority);
-    }
-
-    @Override
-    public void makeNote() {
-
     }
     
     @Override
-    public void reviewNote()
+    public void makeNote()
+    {
+    
+    }
+    
+    @Override
+    public void previewNote()
+    {
+    
+    }
+    
+    @Override
+    public void remind()
     {
     
     }

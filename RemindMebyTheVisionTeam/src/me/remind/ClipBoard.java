095117f152ac297a11018deff1bb198d7d8d1c91
Ivan Rachev <ivan.rachev97@gmail.com>
2018-06-29 16:40:05 +0300

    added archive for notes and data structure for pinned notes

--- RemindMebyTheVisionTeam/src/me/remind/ClipBoard.java
+++ RemindMebyTheVisionTeam/src/me/remind/ClipBoard.java
@@ -1,17 +1,22 @@
 package me.remind;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ClipBoard
 {
     private List<Note> allNotes;
     private List<Remindable> remindableNotes;
+    private List<Note> archivedNotes;
+    private Set<Note> pinnedNotes;
     
     public ClipBoard()
     {
         setAllNotes(new ArrayList<>());
         setRemindableNotes(new ArrayList<>());
+        setPinnedNotes(new HashSet<>());
     }
     
     public List<Note> getAllNotes()
@@ -34,10 +39,27 @@ public class ClipBoard
         this.remindableNotes = remindableNotes;
     }
     
+    public void setPinnedNotes(Set<Note> pinnedNotes)
+    {
+        this.pinnedNotes = pinnedNotes;
+    }
+    
     public void search(String title)
     {
         for (Note note : allNotes)
             if (note.getTitle().equals(title))
                 note.showNote(note);
+    }
+    
+    public void pinNote(Note note)
+    {
+        pinnedNotes.add(note);
+    }
+    
+    public void archiveNote(Note note)
+    {
+        archivedNotes.add(note);
+        
+        allNotes.remove(note);
     }
 }

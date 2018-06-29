2f95edcb76a768dc0b442a52882218d785591d42
Desislava-A <39766396+Desislava-A@users.noreply.github.com>
2018-06-29 08:45:22 +0300

    added private Image photo added constructor with super parameters + Image photo

--- RemindMebyTheVisionTeam/src/me/remind/PhotoNote.java
+++ RemindMebyTheVisionTeam/src/me/remind/PhotoNote.java
@@ -1,6 +1,15 @@
 package me.remind;
 
+import java.awt.*;
+
 public class PhotoNote extends Note {
+    private Image photo;
+
+    PhotoNote(String title, String deadline, Image photo) {
+        super(title, deadline);
+        this.photo=photo;
+    }
+
     @Override
     public void makeNote() {
 

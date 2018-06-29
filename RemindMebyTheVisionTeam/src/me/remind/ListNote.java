c3a21fb3b7cf342815db759957e815b1bd5ec30b
Desislava-A <39766396+Desislava-A@users.noreply.github.com>
2018-06-29 15:05:45 +0300

    added checkboxes as List of Strings , constructors , setter and getter to ListNote class

--- RemindMebyTheVisionTeam/src/me/remind/ListNote.java
+++ RemindMebyTheVisionTeam/src/me/remind/ListNote.java
@@ -1,7 +1,33 @@
 package me.remind;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListNote extends Note
 {
+    private List<String> checkBoxesList;//each index is a new checkbox item
+
+    public List<String> getCheckBoxesList() {
+        return new ArrayList<>(checkBoxesList);
+    }
+
+    public void setCheckBoxesList(List<String> checkBoxesList) {
+        this.checkBoxesList = checkBoxesList;
+    }
+
+    ListNote(String title, String deadline, Priority priority, List<String> checkBoxesList) {
+        this(title, deadline, priority);
+        setCheckBoxesList(new ArrayList<>());
+
+    }
+
+    ListNote(String title, String deadline, Priority priority) {
+        super(title, deadline, priority);
+    }
+
+
+
+
     @Override
     public void makeNote() {
 

8fc9507e167ec8905d397113fae93713233f1eb0
Desislava-A <39766396+Desislava-A@users.noreply.github.com>
2018-06-29 14:36:21 +0300

    added constructors , fields and constants to PhotoNote

--- RemindMebyTheVisionTeam/src/me/remind/PhotoNote.java
+++ RemindMebyTheVisionTeam/src/me/remind/PhotoNote.java
@@ -3,15 +3,53 @@ package me.remind;
 import java.awt.*;
 
 public class PhotoNote extends Note {
-    private Image photo;
+    public static final int MIN_SHORTTXT_LENGHT = 3;
+    public static final int MAX_SHORTTXT_LENGHT = 40;
 
-    PhotoNote(String title, String deadline, Image photo) {
-        super(title, deadline);
+    private Image photo;
+    private String shortText;
+
+    PhotoNote(String title, String deadline, Image photo, Priority priority,String shortText) {
+        this(title, deadline,photo,priority);
         this.photo=photo;
+        setShortText(shortText);
+    }
+
+    PhotoNote(String title, String deadline, Image photo, Priority priority) {
+        super(title, deadline,priority);
+        this.photo=photo;
+        setShortText(null);
+    }
+
+
+
+    private void setShortText(String shortText) {
+        if(shortText==null){
+            System.err.println("Text shuild be between 3 and 60 symbols");
+            return;
+        }
+
+        if(shortText.length()<MIN_SHORTTXT_LENGHT||shortText.length()>MAX_SHORTTXT_LENGHT){
+            System.err.println("Text shuild be between 3 and 60 symbols");
+            return;
+        }
+
+
+        this.shortText = shortText;
     }
 
     @Override
     public void makeNote() {
+
+    }
+
+    @Override
+    public void reviewNote() {
+
+    }
+
+    @Override
+    public void search() {
 
     }
 }

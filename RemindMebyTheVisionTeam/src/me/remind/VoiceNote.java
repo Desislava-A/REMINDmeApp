2d663528703e6fde1de046401fb3bef0ee413da9
Desislava-A <39766396+Desislava-A@users.noreply.github.com>
2018-06-29 15:23:04 +0300

    added field as String audioFile , constructors , setter and getter to VoiceNote class

--- RemindMebyTheVisionTeam/src/me/remind/VoiceNote.java
+++ RemindMebyTheVisionTeam/src/me/remind/VoiceNote.java
@@ -2,6 +2,25 @@ package me.remind;
 
 public class VoiceNote extends Note
 {
+    private  String audioFile;//
+
+    private String getAudioFile() {
+        return audioFile;
+    }
+
+    private void setAudioFile(String audioFile) {
+        this.audioFile = audioFile;
+    }
+
+    public VoiceNote(String title, String deadline, Priority priority, String audioFile) {
+        this(title, deadline, priority);
+        setAudioFile(audioFile);
+    }
+
+    public VoiceNote(String title, String deadline, Priority priority) {
+        super(title, deadline, priority);
+    }
+
     @Override
     public void makeNote() {
 

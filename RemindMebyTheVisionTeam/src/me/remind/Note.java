b379e071f5aeaaad8adac25612248ba61823d916
Desislava-A <39766396+Desislava-A@users.noreply.github.com>
2018-06-29 08:28:51 +0300

    added constructor with title and deadline parameters added override of ToString method

--- RemindMebyTheVisionTeam/src/me/remind/Note.java
+++ RemindMebyTheVisionTeam/src/me/remind/Note.java
@@ -5,7 +5,14 @@ public abstract class Note {
     private String title;
     private String deadline;
 
-    Note() {
+    Note(String title, String deadline) {
+    this.title=title;
+    this.deadline=deadline;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[ %s ] until: %s",title,deadline);
     }
 
     public abstract void makeNote();

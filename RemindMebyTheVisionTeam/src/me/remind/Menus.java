f1a9f45c3993f215602a35bc7ba36054bb82ded2
Ivan Rachev <ivan.rachev97@gmail.com>
2018-07-04 10:51:25 +0300

    deleting redundant submenus

--- RemindMebyTheVisionTeam/src/me/remind/Menus.java
+++ RemindMebyTheVisionTeam/src/me/remind/Menus.java
@@ -20,7 +20,7 @@ public class Menus
         System.out.println("7.Unpin note");
         System.out.println("8.Archive note");
         System.out.println("9.Delete note");
-        System.out.println("10.Clear all notes");
+        System.out.println("10.Clear all");
         System.out.println("11.Clear archive");
         System.out.println("0.Exit");
         
@@ -39,36 +39,6 @@ public class Menus
             }
             
         } while (choice < 0 || choice > 11);
-        
-        return choice;
-    }
-    
-    public static int showSubMenu()
-    {
-        int choice = -1;
-        
-        System.out.println("1.Show all notes");
-        System.out.println("2.View photo notes");
-        System.out.println("3.Play audio notes");
-        System.out.println("4.Change list note items status");
-        System.out.println("0.Exit");
-        
-        Scanner input = new Scanner(System.in);
-        do
-        {
-            System.out.print("\nChoice: ");
-            
-            // Using input.hasNextInt() handles input mismatch errors
-            
-            if (input.hasNextInt())
-                choice = input.nextInt();
-            else
-            {
-                System.err.println("Please choose a number from the menu options");
-                input.next();
-            }
-            
-        } while (choice < 0 || choice > 4);
         
         return choice;
     }

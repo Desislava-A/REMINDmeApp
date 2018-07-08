d74aa620b383d664981e73f96ddc430d521135d2
Desislava-A <39766396+Desislava-A@users.noreply.github.com>
2018-07-08 19:42:48 +0300

    added checks for nonempty line in items of ListNote

--- RemindMebyTheVisionTeam/src/me/remind/ListNote.java
+++ RemindMebyTheVisionTeam/src/me/remind/ListNote.java
@@ -8,46 +8,40 @@ import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class ListNote extends Note implements Remindable
-{
+public class ListNote extends Note implements Remindable {
     public static final int EXIT_INDEX = -1;
     public static final int MIN_LIST_SIZE = 0;
-    
+
     private List<Item> checkBoxesList;
-    
-    public ListNote()
-    {
+
+    public ListNote() {
     }
-    
-    public ListNote(String title, DateTime deadline, Priority priority) throws IOException
-    {
+
+    public ListNote(String title, DateTime deadline, Priority priority) throws IOException {
         super(title, deadline, priority);
         setCheckBoxesList(new ArrayList<>());
         initializeList();
     }
-    
-    protected List<Item> getCheckBoxesList()
-    {
+
+    protected List<Item> getCheckBoxesList() {
         return new ArrayList<>(checkBoxesList);
     }
-    
-    private void setCheckBoxesList(List<Item> checkBoxesList)
-    {
+
+    private void setCheckBoxesList(List<Item> checkBoxesList) {
         this.checkBoxesList = checkBoxesList;
     }
-    
+
     /**
      * Method to initialize the ListNote fields
      */
-    private void initializeList() throws IOException
-    {
+    private void initializeList() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         // When done adding new lines, type "ok" to terminate the adding process
 
-            Queue<String> lines = new LinkedList<>();
-            
-            String newLine ="";
+        Queue<String> lines = new LinkedList<>();
+
+        String newLine;
 
         System.out.println();
 
@@ -55,113 +49,106 @@ public class ListNote extends Note implements Remindable
         System.out.println("For ending the item press: [ENTER]");
         System.out.println("For ending list note press: 0");
 
-        while (true)
-            {
-                newLine = br.readLine();
-                if(newLine.equals("0")){
-                    break;
+        while (true) {
+            newLine = br.readLine();
+
+            //validation for nonemty line
+            if (newLine.isEmpty()) {
+                continue;
             }
 
-            else{
-                    lines.add(newLine);
-                }
-
-
+            if (newLine.equals("0")) {
+                break;
             }
-            
-            // initializing all items as "unchecked"
-            List<Item> checkBoxesAndText = lines.stream()
-                    .map(line ->
-                    {
-                        Item listNote = new Item();
-                        listNote.setItemText(line);
-                        listNote.setCheck(Check.UNCHECKED);
-                        return listNote;
-                    }).collect(Collectors.toList());
-            
-            checkBoxesList = checkBoxesAndText;
+
+            lines.add(newLine);
+
+        }
+
+
+        // initializing all items as "unchecked"
+        List<Item> checkBoxesAndText = lines.stream()
+                .map(line ->
+                {
+                    Item listNote = new Item();
+                    listNote.setItemText(line);
+                    listNote.setCheck(Check.UNCHECKED);
+                    return listNote;
+                }).collect(Collectors.toList());
+
+        checkBoxesList = checkBoxesAndText;
 
     }
-    
-    private void listAllCheckboxItems()
-    {
+
+    private void listAllCheckboxItems() {
         System.out.println("\n[List]");
         checkBoxesList.forEach(x -> System.out.println("\t" + x.toString()));
     }
-    
+
     /**
      * Method that prompts for checking different items from a list note by index
      */
-    protected void promptToChangeStatus()throws IOException
-    {
+    protected void promptToChangeStatus() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
 
-            // when done with marking items checked, type "-1" to terminate the process
-            while (true)
-            {
-                listAllCheckboxItems();
-                System.out.print("\nSelect which index status to change: ");
-                
-                int index = Integer.parseInt(br.readLine());
-                
-                if ((index < MIN_LIST_SIZE || index > checkBoxesList.size()) &&
-                        index != EXIT_INDEX)
-                    return;
-                else if (index == EXIT_INDEX)
-                    return;
-                else
-                {
-                    // on select, always changing the check field to the opposite value
+
+        // when done with marking items checked, type "-1" to terminate the process
+        while (true) {
+            listAllCheckboxItems();
+            System.out.print("\nSelect which index status to change: ");
+
+            int index = Integer.parseInt(br.readLine());
+
+            if ((index < MIN_LIST_SIZE || index > checkBoxesList.size()) &&
+                    index != EXIT_INDEX)
+                return;
+            else if (index == EXIT_INDEX)
+                return;
+            else {
+                // on select, always changing the check field to the opposite value
                     /* substracting 1 because the output ordering starts with 1;
                             => the actual index of the element is minus one */
-                    if (checkBoxesList.get(index - 1).getCheck() == Check.UNCHECKED)
-                        checkBoxesList.get(index - 1).setCheck(Check.CHECKED);
-                    else
-                        checkBoxesList.get(index - 1).setCheck(Check.UNCHECKED);
-                }
+                if (checkBoxesList.get(index - 1).getCheck() == Check.UNCHECKED)
+                    checkBoxesList.get(index - 1).setCheck(Check.CHECKED);
+                else
+                    checkBoxesList.get(index - 1).setCheck(Check.UNCHECKED);
             }
+        }
 
     }
-    
+
     @Override
-    protected String getTitleWithTypeAndPriority()
-    {
+    protected String getTitleWithTypeAndPriority() {
         return "[ListNote] {Priority: " + getPriority().toString().toLowerCase() + "}" +
                 "\n\t\t" + getTitle();
     }
-    
+
     @Override
-    public boolean equals(Object obj)
-    {
+    public boolean equals(Object obj) {
         return obj instanceof ListNote &&
                 ((ListNote) obj).getTitle().equals(getTitle()) &&
                 ((ListNote) obj).getPriority().equals(getPriority()) &&
                 ((ListNote) obj).checkBoxesList.equals(checkBoxesList);
     }
-    
+
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return checkBoxesList.hashCode();
     }
-    
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return getTitle();
     }
-    
+
     @Override
-    protected void showNote()
-    {
+    protected void showNote() {
         System.out.println("\n[List]");
         getCheckBoxesList().forEach(x -> System.out.println("\t" + x));
     }
-    
+
     @Override
-    public void remind()
-    {
+    public void remind() {
         System.out.print(" ->[Reminder set in " + getHoursToDeadline() + " hours]\n");
     }
 }
